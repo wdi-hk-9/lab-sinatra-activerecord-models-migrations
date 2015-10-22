e680a45f508e4525f219a7f92e3465f803943465
Bryan Fong <bryan.fch@gmail.com>
2015-10-23 00:22:36 +0800

    add phone number

--- starter-code/db/schema.rb
+++ starter-code/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151022100706) do
+ActiveRecord::Schema.define(version: 20151022160735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20151022100706) do
     t.integer  "office_number"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "phone_number"
   end
 
   create_table "songs", force: :cascade do |t|
